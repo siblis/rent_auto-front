03caa8f0067cf6c2fed4ec08c4979bce00d5f73b
Shadow <madion86@yandex.ru>
2018-11-10 00:07:15 +0300

    formating

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css/normalize.css';
 import './index.css';
 
-import React, { Fragment } from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import {Provider} from 'react-redux';
+import {Router, Switch, Route} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
 
 import store from './store';
 import routes from './routes';
@@ -16,18 +16,17 @@ import Footer from 'components/Footer';
 export const history = createBrowserHistory();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Fragment>
-                <div className="content">
-                <Header/>
-                <Switch>
-                    {routes.map((route, index) => <Route key={index} {...route}/>)}
-                </Switch>
-                </div>
-                <Footer/>
-            </Fragment>
-        </Router>
-    </Provider>, 
-    document.getElementById('root')
-);
+<Provider store={store}>
+  <Router history={history}>
+    <Fragment>
+      <div className="content">
+        <Header />
+        <Switch>
+          {routes.map((route, index) =>
+          <Route key={index} {...route} />)}
+        </Switch>
+      </div>
+      <Footer />
+    </Fragment>
+  </Router>
+</Provider>, document.getElementById('root'));
