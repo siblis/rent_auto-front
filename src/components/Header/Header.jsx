57d0724ecaa82a640e621a989f2660e5f768b6e9
Shadow <madion86@yandex.ru>
2018-11-10 02:39:54 +0300

    images loader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ export default class Header extends PureComponent {
       <header className="header">
         <Container>
           <nav className="nav">
-            <a href=""><img className="logo" src="src/static/assets/logo.png"></img></a>
+            <a href=""><img className="logo" src="./src/assets/images/logo.png"></img></a>
             <ul className="nav-menu">
               <li>
                 <a href="">Бронирование</a>
