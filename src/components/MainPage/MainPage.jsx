564ee9e1026267b489c43502238066a06e6f95ac
Shadow <madion86@yandex.ru>
2018-11-10 16:49:38 +0300

    page makeup done

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,124 +1,145 @@
 import './MainPage.styl';
 
 import React, { PureComponent } from 'react';
-import { Container, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
+import { Container, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, 
+  Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 export default class Header extends PureComponent {
   state = {
-    dropdownOneOpen: false,   // TO DO: Refactor it (dropdown = component)
-    dropdownTwoOpen: false,
-    dropdownThreeOpen: false,
-    dropdownFourOpen: false,
-    dropdownFiveOpen: false,
-  };
-
-  toggleOne = () => {
-    this.setState(prevState => ({
-      dropdownOneOpen: !prevState.dropdownOneOpen
-    }));
+    carModel: '',
   }
 
-  toggleTwo = () => {
-    this.setState(prevState => ({
-      dropdownTwoOpen: !prevState.dropdownTwoOpen
-    }));
-  }
-
-  toggleThree = () => {
-    this.setState(prevState => ({
-      dropdownThreeOpen: !prevState.dropdownThreeOpen
-    }));
-  }
-
-  toggleFour = () => {
-    this.setState(prevState => ({
-      dropdownFourOpen: !prevState.dropdownFourOpen
-    }));
-  }
-
-  toggleFive = () => {
-    this.setState(prevState => ({
-      dropdownFiveOpen: !prevState.dropdownFiveOpen
-    }));
+  handleCarModel = selectedModel => {
+    this.setState({carModel: selectedModel})
   }
 
   render() {
     return (
       <main className="main-page">
         <Container>
-          <div className="car-info">
-            <div className="car-info__block">
-              <h4>Дата получения</h4>
-              <div className="car-info__input">
-                <object className="car-info__icon" type="image/svg+xml" data={require('../../assets/images/calendar.svg')}></object>
-                <Dropdown isOpen={this.state.dropdownOneOpen} toggle={this.toggleOne}>
-                  <DropdownToggle caret>
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem disabled>Disabled for now</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
+          <Form class="car">
+            <div className="car__info">
+              <div className="car__block">
+                <h4>Дата получения</h4>
+                <div className="car__input">
+                  <object className="car__icon" type="image/svg+xml" data={require('../../assets/images/calendar.svg')}></object>
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem disabled>Disabled for now</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </div>
+              </div>
+              <div className="car__block">
+                <h4>Время получения</h4>
+                <div className="car__input">
+                  <object className="car__icon" type="image/svg+xml" data={require('../../assets/images/clock.svg')}></object>
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem disabled>Disabled for now</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </div>
+              </div>
+              <div className="car__block">
+                <h4>Дата возврата</h4>
+                <div className="car__input">
+                  <object className="car__icon" type="image/svg+xml" data={require('../../assets/images/calendar.svg')}></object>
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem disabled>Disabled for now</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </div>
+              </div>
+              <div className="car__block">
+                <h4>Время возврата</h4>
+                <div className="car__input">
+                  <object className="car__icon" type="image/svg+xml" data={require('../../assets/images/clock.svg')}></object>
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem disabled>Disabled for now</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </div>
+              </div>
+              <div className="car__block">
+                <h4>Марка автомобиля</h4>
+                <div className="car__input">
+                  <object className="car__icon" type="image/svg+xml" data={require('../../assets/images/car.svg')}></object>
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret>
+                      {this.state.carModel}
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem onclick={this.handleCarModel('Renault')}>Renault</DropdownItem>
+                      <DropdownItem onclick={this.handleCarModel('Lada')}>Lada</DropdownItem>
+                      <DropdownItem>Skoda</DropdownItem>
+                      <DropdownItem>BMW</DropdownItem>
+                      <DropdownItem>Toyota</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </div>
               </div>
             </div>
-            <div className="car-info__block">
-              <h4>Время получения</h4>
-              <div className="car-info__input">
-                <object className="car-info__icon" type="image/svg+xml" data={require('../../assets/images/clock.svg')}></object>
-                <Dropdown isOpen={this.state.dropdownTwoOpen} toggle={this.toggleTwo}>
-                  <DropdownToggle caret>
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem disabled>Disabled for now</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
+            <div className="car__details">
+              <div className="car__personal-data">
+                <FormGroup>
+                  <Label for="lastName">Фамилия</Label>
+                  <Input type="text" name="lastName" id="lastName" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="firstName">Имя</Label>
+                  <Input type="text" name="firstName" id="firstName" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="middleName">Отчество</Label>
+                  <Input type="text" name="middleName" id="middleName" />
+                </FormGroup>
+              </div>
+              <div className="car__additional">
+                <div className="car__block">
+                  <h4>Дополнительные услуги</h4>
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem disabled>Disabled for now</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </div>
+                <div className="car__block">
+                  <FormGroup>
+                    <Label for="comment">Комментарий к заказу</Label>
+                    <Input className="car__comment-textarea" type="textarea" name="comment" id="comment" />
+                  </FormGroup>
+                </div>
               </div>
             </div>
-            <div className="car-info__block">
-              <h4>Дата возврата</h4>
-              <div className="car-info__input">
-                <object className="car-info__icon" type="image/svg+xml" data={require('../../assets/images/calendar.svg')}></object>
-                <Dropdown isOpen={this.state.dropdownThreeOpen} toggle={this.toggleThree}>
-                  <DropdownToggle caret>
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem disabled>Disabled for now</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
+            <div className="car__personal-info">
+              <h4>Паспортные данные</h4>
+              <div className="car__documents">
+                <FormGroup>
+                  <Input type="text" name="passport-series" id="passport-series" placeholder="Серия"/>
+                </FormGroup>
+                <FormGroup>
+                  <Input type="text" name="passport-number" id="passport-number" placeholder="Номер"/>
+                </FormGroup>
               </div>
+              <FormGroup>
+                <Input className="car__address-textarea"type="textarea" name="address" id="address" placeholder="Адрес регистрации"/>
+              </FormGroup>
             </div>
-            <div className="car-info__block">
-              <h4>Время возврата</h4>
-              <div className="car-info__input">
-                <object className="car-info__icon" type="image/svg+xml" data={require('../../assets/images/clock.svg')}></object>
-                <Dropdown isOpen={this.state.dropdownFourOpen} toggle={this.toggleFour}>
-                  <DropdownToggle caret>
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem disabled>Disabled for now</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </div>
-            </div>
-            <div className="car-info__block">
-              <h4>Марка автомобиля</h4>
-              <div className="car-info__input">
-                <object className="car-info__icon" type="image/svg+xml" data={require('../../assets/images/car.svg')}></object>
-                <Dropdown isOpen={this.state.dropdownFiveOpen} toggle={this.toggleFive}>
-                  <DropdownToggle caret>
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>Renault</DropdownItem>
-                    <DropdownItem>Lada</DropdownItem>
-                    <DropdownItem>Skoda</DropdownItem>
-                    <DropdownItem>BMW</DropdownItem>
-                    <DropdownItem>Toyota</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </div>
-            </div>
-          </div>
-          <div className="details"></div>
-          <div className="personal-info"></div>
+            <Button className="car__submit">Подать заявку</Button>
+          </Form>
         </Container>
       </main>
     );
